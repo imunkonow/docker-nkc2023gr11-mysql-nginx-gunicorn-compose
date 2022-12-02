c1465122686136eace190c3add962bfd73c296f3
imunkonow <imacatdocomonejp@gmail.com>
2022-12-03 00:08:01 +0900

    gitignoreの追加

---
+++ interview/secret.py
@@ -0,0 +1,1 @@
+subscription = 'e94daa484f894f30989036b8fe9d8a03'
---
+++ interview/static/js/style.js
@@ -0,0 +1,4 @@
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+
+window.open("javascript:window.addEventListener('resize', function (e) {window.resizeTo(300, 300); }, false);", "Good Luck.", "top=368, left=624, width=300, height=300");
