cfe11a4b991a38970daaef0d0264f7422cd2d139
imunkonow <imacatdocomonejp@gmail.com>
2022-12-03 00:13:10 +0900

    gitignore修正

--- interview/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from interview.models import DB
-from interview.models import Account
-
-# Register your models here.
-
-admin.site.register(DB)
-admin.site.register(Account)
--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /mysite
-python manage.py makemigrations
-python manage.py migrate
-gunicorn mysite.wsgi --bind=unix:/var/run/gunicorn/gunicorn.sock
